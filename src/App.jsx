c6284d104aef92df04497d8f4b5b4f135b3387d1
Ramsai <ramsaicrony@gmail.com>
2023-05-15 08:57:51 +0530

    Initialized everything

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Random from "./components/Random";
+import Tag from "./components/Tag";
+
+export default function App() {
+  return (
+    <div className="w-full h-screen flex flex-col background">
+      <h1 className="bg-white rounded-sm">RANDOM GIFS</h1>
+      <div className="flex flex-col">
+        <Random/>
+        <Tag/>
+      </div>
+    </div>
+  );
+}
