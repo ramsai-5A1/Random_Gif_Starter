096c429b70b412aa52bf964c748652166ef858e9
Ramsai <ramsaicrony@gmail.com>
2023-05-24 09:19:56 +0530

    Added Random component

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
       <h1 className="bg-white rounded-lg w-11/12 rounded text-center mt-[40px]
-      px-10 py-2 text-4xl font-bold mx-auto">RANDOM GIFS</h1>
-      <div className="flex flex-col">
+      px-10 py-2 text-4xl font-bold">RANDOM GIFS</h1>
+      <div className="flex flex-col w-full items-center">
         <Random/>
         <Tag/>
       </div>
--- src/components/Random.js
+++ src/components/Random.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Random = () => {
-    return (
-        <div>
+    const [gif, setGif] = useState('');
 
+    const clickHandler = () => {
+        
+    };
+
+
+    return (
+        <div className=" bg-green-500 rounded-lg w-1/2 h-[250px] rounded text-center mt-[40px] px-10 py-2">
+            <h1 className="underline">A RANDOM GIF</h1>
+            <img src={gif} width='450' />
+            <button onClick={clickHandler}>GENERATE</button>
         </div>
     );
 }
